61b4c5e2979e8296291eddfb7fc88cca3b37a36c
yj95228 <kei02119@naver.com>
2022-07-07 22:27:52 +0900

    chore: prettier endOfLine 에러 시 auto 규칙 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
-const createConfig = require("@titicaca/eslint-config-triple/create-config");
+const createConfig = require('@titicaca/eslint-config-triple/create-config')
 
-module.exports = createConfig({ type: "frontend", project: "./tsconfig.json" });
+const { extends: extendConfigs, overrides } = createConfig({
+  type: 'frontend',
+  project: './tsconfig.json',
+})
+
+module.exports = {
+  extends: [...extendConfigs],
+  overrides: [...overrides],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+}
